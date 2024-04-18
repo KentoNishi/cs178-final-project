d94f5576e5d801daa8d2f682302b78c668fd2288
John Hwang <johnhwang@college.harvard.edu>
2024-04-18 16:28:50 -0400

    fixed an error

--- backend/custom_retriever.py
+++ backend/custom_retriever.py
@@ -9,8 +9,7 @@ class CustomRetriever(BaseRetriever):
     def get_relevant_documents(self, query):
         # calculate embeddings for query string
         emb = self.embeddings.embed_query(query)
-        option1 = self.chroma.similarity_search_by_vector(embedding=emb, lambda_mult = 0.6)
-        option2 = self.chroma.max_marginal_relevance_search_by_vector(embedding=emb, lambda_mult=0.8)
+        option1 = self.chroma.similarity_search_by_vector(embedding=emb)
         return option1
     
     async def aget_relevant_documents(self):
