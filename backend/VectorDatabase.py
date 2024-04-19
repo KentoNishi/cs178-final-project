348df7d7b6029df9626231c89fe8a0c59e239438
Andrew Holmes <acholmes001@gmail.com>
2024-04-19 00:57:22 -0400

    woops, was using wrong embedding function in VectorDatabase class, fixed

--- backend/VectorDatabase.py
+++ backend/VectorDatabase.py
@@ -19,7 +19,7 @@ class VectorDatabase:
         # OpenAI Embedding Function
         self.embedding_function = embedding_functions.OpenAIEmbeddingFunction(
             api_key=os.getenv("OPENAI_API_KEY"),
-            model_name="text-embedding-ada-002",
+            model_name="text-embedding-3-small",
         )
 
         # Store ChromaDB persistent path
